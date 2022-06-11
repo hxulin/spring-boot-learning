3a695ddb63e6ea49b11948a90db9fc19aea50fce
hxulin <1455936807@qq.com>
2022-06-11 08:37:39 +0800

    注释完善

--- thread-pool/src/main/java/cn/huangxulin/learning/threadpool/controller/AsyncController.java
+++ thread-pool/src/main/java/cn/huangxulin/learning/threadpool/controller/AsyncController.java
@@ -1,11 +1,14 @@
 package cn.huangxulin.learning.threadpool.controller;
 
+import cn.huangxulin.learning.threadpool.config.ThreadPoolConfig;
 import cn.huangxulin.learning.threadpool.service.AsyncService;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Future;
 
 /**
  * @author hxl
@@ -16,11 +19,6 @@ public class AsyncController {
 
     private final AsyncService asyncService;
 
-    /**
-     * 使用异步任务，不等待返回结果（几毫秒）
-     *
-     * @return ignored
-     */
     @GetMapping("/async/test1")
     public Object test1() {
         long start = System.currentTimeMillis();
@@ -30,12 +28,24 @@ public class AsyncController {
         return "test1";
     }
 
+    /**
+     * 测试异常处理，当异步任务返回 {@link Future} 时，
+     * 不会触发 {@link ThreadPoolConfig#getAsyncUncaughtExceptionHandler()} 异常处理
+     *
+     * @return ignored
+     */
     @GetMapping("/async/test2")
     @SneakyThrows
     public Object test2(@RequestParam Integer number) {
         return asyncService.testException(number).get();
     }
 
+    /**
+     * 测试异常处理，当异步任务返回 void 时，
+     * 会触发 {@link ThreadPoolConfig#getAsyncUncaughtExceptionHandler()} 异常处理
+     *
+     * @return ignored
+     */
     @GetMapping("/async/test3")
     public Object test3(@RequestParam Integer number) {
         asyncService.testVoidException(number);
